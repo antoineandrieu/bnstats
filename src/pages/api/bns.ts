2bc68bf4befca6b28c657b513c799de1c8df0dcd
Antoine Andrieu <antoine@scircula.com>
2022-01-16 12:23:53 +0100

    chore: move components folder to src

--- src/pages/api/bns.ts
+++ src/pages/api/bns.ts
@@ -4,10 +4,9 @@ type Data = {
   expDate: string;
 };
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
+const handler = (req: NextApiRequest, res: NextApiResponse<Data>) => {
   const expDate = new Date().toLocaleString();
   res.status(200).json({ expDate });
-}
+};
+
+export default handler;
